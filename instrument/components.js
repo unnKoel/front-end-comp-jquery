054f7bd2d9c815027b062c77675220a744238fda
unnKoel <unnkoel@gmail.com>
2016-08-09 18:09:29 +0800

    add components.js

---
+++ instrument/components.js
@@ -0,0 +1,21 @@
+/**
+ * Created by common on 2016/8/9.
+ */
+(function (win) {
+  /**
+   * easyDialog支持ie6
+   */
+  var easyDialog = function () {
+    return {
+
+      open: function () {
+
+      },
+
+
+      close: function () {
+
+      }
+    };
+  }
+})(window);
